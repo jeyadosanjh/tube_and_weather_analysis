4be07e31c37f546ff54ce263662f44b22f4aa2bc
jeyadosanjh <jeya.dosanjh13@gmail.com>
2025-12-28 20:16:14 +0000

    first function and test

--- basic_function.py
+++ basic_function.py
@@ -1,1 +1,5 @@
 
+def increment(x):
+    """Returns the input value incremented by 1."""
+    return x + 1
+
--- test_basic_function.py
+++ test_basic_function.py
@@ -1,1 +1,11 @@
+import unittest
+from basic_function import increment
 
+class TestBasicFunction(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(increment(3), 4)
+        self.assertEqual(increment(-1), 0)
+        self.assertEqual(increment(0), 1)
+
+if __name__ == '__main__':
+    unittest.main()
